8a870ba29bb6e466b8ce7d99737d41f7035ea91e
devpedroferreira <pedroofan@gmail.com>
2025-01-11 15:07:44 -0300

    clone-apple fineshed

---
+++ 2025/apple-clone/script.js
@@ -0,0 +1,11 @@
+
+function changeColor(cor){
+    let circle = document.querySelector('.circle');
+    circle.style.backgroundColor = cor;
+};
+
+
+function changeImagem(img){
+    let imagemIphone = document.querySelector('.main-img');
+    imagemIphone.src = img;
+}
