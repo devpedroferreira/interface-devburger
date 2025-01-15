3062488bb209acdfcbb07a610b21b3dcbfb294d1
devpedroferreira <pedroofan@gmail.com>
2025-01-14 23:09:42 -0300

    mongoDB+prisma instaled

--- 2025/Node-Js/lesson_1_conceitos/server.js
+++ 2025/Node-Js/lesson_1_conceitos/server.js
@@ -4,11 +4,26 @@
 
 // jeito novo
 // no package.json adicionar "type": "module",
-import express from 'express';
+import express from 'express';  
+// user:pedroofan pass: 1JvVusFqL0QFfG8n // acess mongodb atlas
+
+
+
+
 // start server
 const app = express();
 app.use(express.json()); // para o express entender json BODY
-app.listen(3333, () => console.log('Server is running on port 3333!'));
+
+// array simule database
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Daniel'
+];
+
+app.listen(3333);
+//app.listen(3333, () => console.log('Server is running on port 3333!'));
 
 // protocolo http
 /*
@@ -23,13 +38,24 @@ app.listen(3333, () => console.log('Server is running on port 3333!'));
 // http://localhost:3333/user - reposta quando chegar essa resiscao/site
 // response to client: mensagem Hello World
 // rota get no servidor responde o front-end cliente
-app.get('/user', (request, response) => {
-    response.send( "Hello World my friend!" );
-}); // rota   
+// aqui no get apos o diretorio /users/: significa o diretorio que o front-end vai acessar
+app.get('/users', (req, res) => {
+    res.status(200).json(users); // resposta para o front-end
+}); 
+
+
+// rota   
 
 // rota POST
-app.post('/user', (request, response) => {
-    response.send('resp POST for thunder');
+app.post('/users', (req, res) => {
+    users.push(req.body); // add nome no array
+    //console.log(req.breste POST"); // para ver o body content
+    res.status(201).json({message: "User created with success!"}); // resposta para o front-end   
 });
+
+// lesson 12 creat user simulator on variable
+// install biblioteca prisma io
+
+
 
 
