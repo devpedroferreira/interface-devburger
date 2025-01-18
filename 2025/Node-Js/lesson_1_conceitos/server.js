b7635634aa470dddf558b948a7285fd2b9aa2474
devpedroferreira <pedroofan@gmail.com>
2025-01-17 23:50:54 -0300

    finalizado modulo nodeJS

--- 2025/Node-Js/lesson_1_conceitos/server.js
+++ 2025/Node-Js/lesson_1_conceitos/server.js
@@ -1,61 +1,72 @@
-// jeito antigo 
-// no package.json adicionar "type": "commonjs", 
-//const express = require('express');
-
-// jeito novo
-// no package.json adicionar "type": "module",
-import express from 'express';  
-
-// mongodb+srv://pedroofan:<db_password>@users.6suw3.mongodb.net/?retryWrites=true&w=majority&appName=users
-// user:pedroofan pass: 1JvVusFqL0QFfG8n // acess mongodb atlas
-
-
-// start server
+// Date: 2021-17-01
+//import express
+import express from 'express';
 const app = express();
-app.use(express.json()); // para o express entender json BODY
+app.use(express.json());// para express funcionar .json dentro da rota post
 
-// array simule database
-const users = [
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Daniel'
-];
+// para funcionar o message .json dentro da rota post
+import pkg from 'statuses';
+const { message } = pkg;
 
-app.listen(3333);
-//app.listen(3333, () => console.log('Server is running on port 3333!'));
+// import prisma client
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient();
 
-// protocolo http
-/*
-    get: buscar informacao
-    post: criar informacao
-    put: editar varias informacao
-    push: editar uma informacao
-    delete: deletar informacao
-
-*/
-
-// http://localhost:3333/user - reposta quando chegar essa resiscao/site
-// response to client: mensagem Hello World
-// rota get no servidor responde o front-end cliente
-// aqui no get apos o diretorio /users/: significa o diretorio que o front-end vai acessar
-app.get('/users', (req, res) => {
-    res.status(200).json(users); // resposta para o front-end
-}); 
-
-
-// rota   
-
-// rota POST
-app.post('/users', (req, res) => {
-    users.push(req.body); // add nome no array
-    //console.log(req.breste POST"); // para ver o body content
-    res.status(201).json({message: "User created with success!"}); // resposta para o front-end   
+// porta do servidor
+app.listen(3333, () => {
+  console.log(' -> Server STARTED on port 3333 <-');
 });
 
-// lesson 12 creat user simulator on variable
-// install biblioteca prisma io
+// rota get 
+// route params roda no get  add / apos diretorio
+// http://localhost:3333/users/31321s21
+// o diretorio configura aqui no back-end
+// app.get('/users/:id', (req, res) => {
 
+app.get('/users', async (req, res) => { //route ativa /:acabate
+      
+  const allUsers = await prisma.user.findMany();
+  res.status(200).json(allUsers);
+}); 
 
+// rota post
+app.post('/users', async (req, res)  => {
+    
+      const userCreated =  await prisma.user.create({
+              data: {
+                  name: req.body.name,
+                  email: req.body.email,
+                  age:  req.body.age
+              }
+        });
 
+    res.status(201).json(userCreated);
+});
+
+// route update
+app.put('/users/:id', async (req, res) => {
+  
+  const userUpdated = await prisma.user.update({
+    where: { id: req.params.id },
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age
+    }
+  });
+
+  res.status(200).json(userUpdated);
+
+});
+
+//route delete  
+app.delete('/users/:id', async (req, res) => {
+  
+  const userDeleted = await prisma.user.delete({
+    where: { id: req.params.id }
+  });
+
+  res.status(200).json(userDeleted);
+
+});
 
