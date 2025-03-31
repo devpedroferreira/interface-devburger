312e93db87050100ab6b8855b541814d3a1f46fa
devpedroferreira <pedroofan@gmail.com>
2025-03-31 00:06:05 -0300

    Create /Home/index.jsx and styles.js

---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,15 @@
+export function Home() {
+  return (
+    <main>
+        <Banner>
+            title="Bem vindo ao Burger Boss"
+        </Banner>
+        <Container>
+            <Content>
+                <div>Categorias</div>
+                <div>Produtos</div>
+            </Content>
+        </Container>
+    </main>
+  );
+}
