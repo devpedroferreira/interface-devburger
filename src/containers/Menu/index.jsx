4f21df0dfb03390b39d11cc7792c3a958b922039
devpedroferreira <pedroofan@gmail.com>
2025-04-30 11:29:35 -0300

    creating menu structure and adding /cardapio route

--- src/containers/Menu/index.jsx
+++ src/containers/Menu/index.jsx
@@ -1,41 +1,31 @@
-import { Container } from './styles';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { setUser } from '../../store/userSlice';
-import { useEffect } from 'react';
+import { Container, BannerMenu, CategoryMenu, ProductsContainer } from './styles';
 
 export function Menu() {
-    const [isOpen, setIsOpen] = useState(false);
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    
-    const handleMenuToggle = () => {
-        setIsOpen(!isOpen);
-    };
-    
-    const handleLogout = () => {
-        dispatch(setUser(null));
-        navigate('/login');
-    };
-    
-    useEffect(() => {
-        if (isOpen) {
-        document.body.style.overflow = 'hidden';
-        } else {
-        document.body.style.overflow = 'auto';
-        }
-    }, [isOpen]);
-    
     return (
-        
-        <Container isOpen={isOpen}>
-        <button onClick={handleMenuToggle}>Menu</button>
-        {isOpen && (
-            <div className="menu-content">
-            <button onClick={handleLogout}>Logout</button>
-            </div>
-        )}
+        <Container>
+            <BannerMenu>
+                <h1>O melhor
+                
+                está aqui!</h1>
+            
+            </BannerMenu>
+
+            <CategoryMenu>
+                
+            </CategoryMenu>
+            <ul>
+                <li>Hamburguer</li>
+                <li>Pizza</li>
+                <li>Pastel</li>
+                <li>Combos</li>
+            </ul>
+            <ProductsContainer>
+
+
+            </ProductsContainer>
+                
+
+
         </Container>
     );
-    }
+}
---
+++ src/containers/Menu/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import BannerCardapio from '../../assets/BgBannerMenu.svg';
+
+
+export const Container = styled.div`
+    
+`;
+
+export const BannerCardapio = styled.div`
+    background-image: url(${BannerCardapio});
+    width: 100%;
+`;
+
+
+export const CategoryMenu = styled.div`
+    
+`;
+
+
+export const ProductsContainer = styled.div`
+    
+`;
